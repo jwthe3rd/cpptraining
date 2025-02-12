d3e830259c1cabf598c7e2373885add285eac974
John <johnwhlte@proton.me>
2025-02-12 07:40:18 -0600

    day 6: fixed the quadtree implementation to be much more correct

--- d_5/run.cpp
+++ d_5/run.cpp
@@ -1,18 +1,23 @@
 #include "utils.H"
 
+#ifndef T
+#define T = float;
+#endif
+
 int main()
 {
 
     // Instantiate particle list
-    int numParticles = 100;
+    int numParticles = 10000;
 
-    LinkedList<Particle> particleSystem;
+
+    LinkedList<Particle<float>> particleSystem;
 
     for (int i = 0; i < numParticles; i++)
     {
-        Particle p;
-        p.x = 0.01*i;
-        p.y = 0.01*i/2;
+        Particle<float> p;
+        p.x = 0.0001*i;
+        p.y = 0.0001*i/2;
         p.r = 0.01;
         particleSystem.insertAtEnd(p);
     }
@@ -26,35 +31,19 @@ int main()
 
     float allParticleRadii = 0.02f;
 
-    Box rootNode;
+    QuadTree<float> qt(Box(origin, origin, render_view_x, render_view_y));
 
-    rootNode.x_l = origin;rootNode.y_l = origin;
-    rootNode.x_u = render_view_x; rootNode.y_u = render_view_y;
+    Node<Particle<float>>* temp = particleSystem.head;
 
-    int maxParticleNumber = 4;
-
-    QuadTree qt(rootNode);
-
-    qt.addParticles(particleSystem, maxParticleNumber);
-
-    //cout << qt.quadtree.size() << endl;
-
-    for (Box elem : qt.quadtree)
+    while (temp)
     {
-        cout << "Parent: " << elem.x_u << ", " << elem.x_l << ", " << elem.y_u << ", " << elem.y_l;
-        if (elem.children.size()==0)
-        {cout << " --> Leaf Node!" << endl;}
-        else{
-            cout << endl;
-        for (Box elem2 : elem.children)
-        {
-
-        cout << "Child: -----> " << elem2.x_u << ", " << elem2.x_l << ", " << elem2.y_u << ", " << elem2.y_l << endl;
-        }}
+        qt.insert(temp->data);
+        temp = temp->next;
     }
 
+    cout << "QuadTree Points: " << endl;
 
-
+    qt.print();
 
 
     return 0;
